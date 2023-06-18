11a4e40216cf1c4d35ab476376cadbf8b1103814
Iron-Baloou <marco.jan@gmx.de>
2023-06-18 22:20:17 +0200

    init commit

--- main2.js
+++ main2.js
@@ -1,42 +1,39 @@
+
 let form = document.getElementById('add_form');
 let add_btn_form = document.getElementById('add_btn2');
 let read_btn = document.getElementsByClassName('text_field');
 let open_form = document.getElementById('add_btn');
 
-
 // Event Listener hinzufügen
 open_form.addEventListener("click", showForm);
 add_btn_form.addEventListener("click", addBook);
+
+
+
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    
+   
 }
 
+function showForm() {
+    console.log("Sie haben geklickt");
+    form.style.display = "block";
+}
 
 function addBook() {
     let titleInput = document.querySelector('#add_form input:nth-of-type(1)').value;
     let authorInput = document.querySelector('#add_form input:nth-of-type(2)').value;
     let pagesInput = document.querySelector('#add_form input:nth-of-type(3)').value;
 
-    let pagestest = /^[0-9]+$/;
 
 
-    let alphanumerictest = /^[a-zA-Z0-9!?.:()+-;" "]+$/;
-    if (!pagestest.test(pagesInput)) {
-        window.alert("Please enter a number for the pages");
-    }
-    else if (!alphanumerictest.test(titleInput) || !alphanumerictest.test(authorInput)) {
-        window.alert("Please enter a valid title and author");
+    let newBook = new Book(titleInput, authorInput, pagesInput);
 
-    } else {
-        let form = document.getElementById("add_form");
-
-        let readCheckbox = document.querySelector('#add_form input[type="checkbox"]').checked;
-        let newBook = new Book(titleInput, authorInput, pagesInput, readCheckbox);
-
+        console.log(titleInput);
 
         let bookshelf = document.getElementById("bookshelf");
         let book = document.createElement("div");
@@ -62,39 +59,13 @@ function addBook() {
         read.textContent = newBook.read ? "read" : "no read";
         book.appendChild(read);
 
-        let readText = document.createElement("span");
-        readText.className = "read_text";
-        readText.textContent = "read or not read";
-        book.appendChild(readText);
 
-        let checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.id = "modi_checkbox";
-        checkbox.checked = newBook.read;
-        checkbox.addEventListener("change", function () {
-            if (this.checked) {
-                read.textContent = "read";
-            } else {
-                read.textContent = "not read";
-            }
-        });
-        book.appendChild(checkbox);
-
-        let removeButton = document.createElement("button");
-        removeButton.className = "remove_button";
-        removeButton.textContent = "delete";
-        removeButton.addEventListener("click", function () {
-            book.remove();
-        });
-        book.appendChild(removeButton);
-
-        showForm();
+        // ende
         clearForm();
-
         form.style.display = "none";
 
-    }
 }
+
 
 function clearForm() {
     document.querySelector('#add_form input:nth-of-type(1)').value = "";
@@ -110,6 +81,5 @@ function showForm() {
     let form = document.getElementById("add_form");
     form.style.display = "flex";
 }
-
 
 
