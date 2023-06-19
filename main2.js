aebaec1067eb0ef4c9990f3e86edf6151d1d761b
Iron <marco.jan@gmx.de>
2023-06-19 13:51:51 +0200

    init commit

--- main2.js
+++ main2.js
@@ -1,45 +1,58 @@
 
-let form = document.getElementById('add_form');
-let add_btn_form = document.getElementById('add_btn2');
-let read_btn = document.getElementsByClassName('text_field');
-let open_form = document.getElementById('add_btn');
+//book objekt (kontruktor)
+function Book(title, author, pages, read, bookId) {
 
-// Event Listener hinzufügen
-open_form.addEventListener("click", showForm);
-add_btn_form.addEventListener("click", addBook);
-
-
-
-
-function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-   
+
+    this.bookId = bookId;
+
 }
 
-function showForm() {
-    console.log("Sie haben geklickt");
-    form.style.display = "block";
-}
+//funktion um die book id hochzuzählen
+function bookaddid() {
+    let bookId = "a"+1;
+    let book = document.querySelectorAll(".book");
+    for (let i = 0; i < book.length; i++) {
+      book[i].id = bookId;
+      bookId++;
+    }
+  }
 
-function addBook() {
+
+
+/* bucheinbinde  */
+function addBook(newbook) {
     let titleInput = document.querySelector('#add_form input:nth-of-type(1)').value;
     let authorInput = document.querySelector('#add_form input:nth-of-type(2)').value;
     let pagesInput = document.querySelector('#add_form input:nth-of-type(3)').value;
 
+    let pagestest = /^[0-9]+$/;
 
 
-    let newBook = new Book(titleInput, authorInput, pagesInput);
+    let alphanumerictest = /^[a-zA-Z0-9!?.:()+-;" "]+$/;
+    if (!pagestest.test(pagesInput)) {
+        window.alert("Please enter a number for the pages");
+    }
+    else if (!alphanumerictest.test(titleInput) || !alphanumerictest.test(authorInput)) {
+        window.alert("Please enter a valid title and author");
 
-        console.log(titleInput);
+    } else {let bookIdCounter = 1;
+        let form = document.getElementById("add_form");
+
+        let readCheckbox = document.querySelector('#add_form input[type="checkbox"]').checked;
+        let newBook = new Book(titleInput, authorInput, pagesInput, readCheckbox);
+
 
         let bookshelf = document.getElementById("bookshelf");
         let book = document.createElement("div");
         book.className = "book"
 
         bookshelf.appendChild(book);
+        
+
 
         let title = document.createElement("h2");
         title.textContent = newBook.title;
@@ -53,18 +66,80 @@ function addBook() {
         pages.className = "pages";
         pages.textContent = newBook.pages;
         book.appendChild(pages);
-
+    
         let read = document.createElement("p");
-        read.className = "read";
+        read.className = "let bookIdCounter = 1;read";
         read.textContent = newBook.read ? "read" : "no read";
         book.appendChild(read);
 
+        let readText = document.createElement("span");
+        readText.className = "read_text";
+        readText.textContent = "read or not read";
+        book.appendChild(readText);
 
-        // ende
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.id = "modi_checkbox";
+        checkbox.checked = newBook.read;
+        checkbox.addEventListener("change", function () {
+            if (this.checked) {
+                read.textContent = "read";
+            } else {
+                read.textContent = "not read";
+            }
+        });
+        book.appendChild(checkbox);
+
+        
+
+        let removeButton = document.createElement("button");
+        removeButton.className = "remove_button";
+        removeButton.textContent = "delete";
+        removeButton.addEventListener("click", function () {
+            book.remove();
+        });
+        book.appendChild(removeButton);
+
+        bookaddid();
+        showForm();
         clearForm();
-        form.style.display = "none";
+        
 
+        function Bookshelf() {
+            this.books = [];
+            
+        }
+        
+        Bookshelf.prototype.addBook = function(book){
+            this.books.push(book);
+            localStorage.setItem('library', JSON.stringify(this.books))
+        }
+        
+        const bookShelf = new Bookshelf()
+
+        form.style.display = "none";
+        console.log("#2");
+
+        
+    }
 }
+
+
+
+
+/* 
+const booksarray = [];
+
+function addtoarr(arr, newbook) {
+  arr.push(newbook);
+}
+
+
+addtoarr(booksarray, newbook);
+
+console.log(booksarray);
+
+ */
 
 
 function clearForm() {
@@ -81,5 +156,6 @@ function showForm() {
     let form = document.getElementById("add_form");
     form.style.display = "flex";
 }
+
 
 
---
+++ test.js
@@ -0,0 +1,21 @@
+/* //const book = 4;
+const booksarray = []
+
+function addsome(booksarray){
+
+const count = booksarray.push('hallo');
+    
+}
+
+console.log(booksarray); */
+
+const booksarray = [];
+
+function addsome(arr) {
+  const count = arr.push('hallo');
+}
+
+addsome(booksarray);
+console.log(booksarray);
+
+
